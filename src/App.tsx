66df3a60dea44e058a32d40d44c258828d2e7432
Aaron Green <aarongreen1997@gmail.com>
2023-06-30 13:37:10 -0500

    add dropdown menu

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,25 @@ function App() {
       <div className="main flexrow scanner">
         <h1 className="text-5xl my-6 font-bold">FTERM</h1>
         <form onSubmit={(e) => handleWordEntries(e)}>
+          <label htmlFor="wordLen">Word Length:</label>
+          <select
+            name="wordLen"
+            id="wordLen"
+            className="focus:outline-none pl-5 py-3 bg-gray-800"
+          >
+            <option value="4">Four</option>
+            <option value="5">Five</option>
+            <option value="6">Six</option>
+            <option value="7">Seven</option>
+            <option value="8">Eight</option>
+            <option value="9">Nine</option>
+            <option value="10">Ten</option>
+            <option value="11">Eleven</option>
+            <option value="12">Twelve</option>
+            <option value="13">Thirteen</option>
+            <option value="14">Fourteen</option>
+            <option value="15">Fifteen</option>
+          </select>
           <label htmlFor="words" className="text-xl block">
             input terminal words separated by a space:
           </label>
