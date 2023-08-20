489e2b4b2d7b32faf309fae80086221bc5f79932
Aaron Green <aarongreen1997@gmail.com>
2023-08-19 22:06:48 -0500

    fix incorrectly calculating word length bug

--- src/components/GuessEntryForm.tsx
+++ src/components/GuessEntryForm.tsx
@@ -10,12 +10,18 @@ const GuessEntryForm = () => {
     },
     dispatch,
   } = useContext(WordHistoryContext);
-  const wordLength = words.reduce(
-    (n) => {
-      return [...n, n.length];
-    },
-    [0, 1]
-  );
+
+  const getWordLengthArray = () => {
+    const hasWord = words.length > 0;
+    return hasWord
+      ? [...words[0]].reduce(
+          (n) => {
+            return [...n, n.length];
+          },
+          [0]
+        )
+      : [];
+  };
 
   const guessAlreadyExists = () =>
     guesses.find((g) => g.guessName === selectedEntry);
@@ -37,9 +43,9 @@ const GuessEntryForm = () => {
 
   return (
     <>
-      <p className="mt-10 mb-2 text-lg md:text-xl">NUMBER CORRECT</p>
+      <p className="mb-2 mt-10 text-lg md:text-xl">NUMBER CORRECT</p>
       <div className="wordsGrid">
-        {wordLength.map((n) => (
+        {getWordLengthArray().map((n) => (
           <div key={selectedEntry + n} className="mb-4 flex justify-center">
             <button
               className="w-full rounded-md border-2 border-black px-5 py-3 hover:bg-stone-500"
